f12ccdde0db4f904b09a6aa4194bb06099201984
Herman Neple <hermanneple@Hermans-MacBook-Pro.local>
2019-08-17 22:01:28 +0200

    Added base of game with the movement of player

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
 //Start of main.js
+let y = 435;
+
+
+
+
+function setup(){
+  createCanvas(500, 500);
+  noCursor();
+}
+
+
+function update(){
+
+}
+
+function draw(){
+  update();
+  background(51);
+  //Here, the graphics are drawn
+
+
+  //this is the player
+  rect(mouseX - 25, y, 50, 50);
+}
